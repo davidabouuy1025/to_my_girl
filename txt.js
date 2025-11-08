1bf3ea18f581f8d80c025dffa683196413abf0a2
leejunming35622792 <jlee0465@student.monash.edu>
2025-11-08 18:20:05 +0800

    Update text page

---
+++ txt.js
@@ -0,0 +1,42 @@
+const texts = [
+    "> ...",
+    "> print(datetime.datetime.now())",
+    "> 2025-12-12T05:20:00.131400",
+    "> for friend in my_friends:",
+    ">      if friend['bday']:",
+    ">          print(`It's {friend['name']}'s bday!`)",
+    "> ...",
+    "> ...",
+    "",
+    "It's WHO's bday!"
+];
+
+const output = document.getElementById("output");
+
+// Helper to pause
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runFirstScript() {
+    output.innerHTML = ""; // clear previous text
+    for (let i = 0; i < texts.length; i++) {
+        output.innerHTML += texts[i] + "<br>";
+        await wait(1000); // wait 1 second between lines
+    }
+
+    // Trigger confetti at the end
+    confetti({ particleCount: 150, spread: 70, origin: { y: 0.6 } });
+    await wait(3000)
+
+    window.location.href = "bday.html";
+
+    // Replace current page in history (cannot go back)
+    // window.location.replace("bday.html");
+
+    // Reload current page
+    // window.location.reload();
+}
+
+// Start
+runFirstScript();
